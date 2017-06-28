c51e91d8911366c96090f1be85b9ecb7f4f75acf
Mike <zellermike@icloud.com>
2017-06-28 15:27:53 +0200

    Auto stash before merge of "master" and "origin/master"

--- plugins/pacman/pacmanplugin.cpp
+++ plugins/pacman/pacmanplugin.cpp
@@ -1,4 +1,4 @@
-#include "pacmanplugin.h"
+
 
 #include <QDebug>
 #include <QProcess>
@@ -52,6 +52,9 @@ QStringList PacmanPlugin::listPackages(QVector<bool> extraFilters)
 	if(!p.waitForFinished(5000))
 		return {};
 
+	auto list = QString::fromUtf8(p.readAll()).split(QStringLiteral("\n"), QString::SkipEmptyParts);
+	qDebug() << list;
+	return list;
 	return QString::fromUtf8(p.readAll()).split(QStringLiteral("\n"), QString::SkipEmptyParts);
 }
 
