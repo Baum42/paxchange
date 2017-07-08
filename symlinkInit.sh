922f0c519653fe1e23868f05b8ca393e16c68ca2
Skycoder42 <Skycoder42@users.noreply.github.com>
2017-07-08 15:46:40 +0200

    toplevel rename

--- symlinkInit.sh
+++ symlinkInit.sh
@@ -5,19 +5,19 @@ srcDir=$(pwd)
 #mime
 mkdir -p ~/.local/share/mime/packages/
 cd ~/.local/share/mime/packages/
-sudo ln -s "$srcDir/gui/application-x-pacsync-database.xml"  && update-mime-database ~/.local/share/mime
+sudo ln -s "$srcDir/gui/application-x-paxchange-database.xml"  && update-mime-database ~/.local/share/mime
 
 #hooks
 cd /usr/share/libalpm/hooks
-sudo ln -s "$srcDir/plugins/pacman/pacsync_install.hook"
+sudo ln -s "$srcDir/plugins/pacman/paxchange_install.hook"
 
 cd /usr/share/libalpm/hooks
-sudo ln -s "$srcDir/plugins/pacman/pacsync_remove.hook"
+sudo ln -s "$srcDir/plugins/pacman/paxchange_remove.hook"
 
 #hook script
 cd /usr/share/libalpm/scripts
-sudo ln -s "$srcDir/plugins/pacman/pacsync.sh"
+sudo ln -s "$srcDir/plugins/pacman/paxchange.sh"
 
 #bin
 cd /usr/bin
-sudo ln -s "$srcDir/pacsync"
+sudo ln -s "$srcDir/paxchange"
