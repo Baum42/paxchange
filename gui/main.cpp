37a4e16e1a831d3616d6491ef541bf42ea145581
Skycoder42 <Skycoder42@users.noreply.github.com>
2018-06-09 18:06:10 +0200

    made stdin args possible

--- gui/main.cpp
+++ gui/main.cpp
@@ -14,7 +14,40 @@ static void setupParser(QCommandLineParser &parser);
 
 int main(int argc, char *argv[])
 {
-	QApplication a(argc, argv);
+	// ugly fix to enable stdin input
+	QByteArrayList xArgs;
+	int stdIdx = -1;
+	for(auto i = 0; i < argc; i++) {
+		if(qstrcmp(argv[i], "--stdin") == 0) {
+			stdIdx = i;
+			QFile in;
+			in.open(stdin, QIODevice::ReadOnly);
+			xArgs = in.readAll().simplified().split(' ');
+			break;
+		}
+	}
+
+	QVector<char*> nArgs{argc + xArgs.size()};
+	int nArgc = 0;
+	char **nArgv = nullptr;
+	if(xArgs.isEmpty()) {
+		nArgs.clear();
+		nArgc = argc;
+		nArgv = argv;
+	} else {
+		int idx;
+		for(idx = 0; idx < argc; idx++)
+			nArgs[idx] = argv[idx];
+		for(; (idx - argc) < xArgs.size(); idx++)
+			nArgs[idx] = xArgs[idx - argc].data();
+		nArgs.removeAt(stdIdx);
+
+		nArgc = nArgs.size();
+		nArgv = nArgs.data();
+	}
+
+	// begin actual main
+	QApplication a(nArgc, nArgv);
 	QApplication::setApplicationName(QStringLiteral(TARGET));
 	QApplication::setApplicationVersion(QStringLiteral(VERSION));
 	QApplication::setOrganizationName(QStringLiteral(COMPANY));
@@ -22,6 +55,8 @@ int main(int argc, char *argv[])
 	QApplication::setApplicationDisplayName(QStringLiteral(DISPLAY_NAME));
 	QApplication::setWindowIcon(QIcon(QStringLiteral(":/icons/main.svg")));
 	QApplication::setQuitOnLastWindowClosed(false);
+
+	qDebug() << QCoreApplication::arguments();
 
 	//load translations
 	DatabaseController::loadTranslation(QStringLiteral("paxchange_gui"));
@@ -62,7 +97,7 @@ int main(int argc, char *argv[])
 		return EXIT_SUCCESS;
 	});
 
-	QObject::connect(&instance, &QSingleInstance::instanceMessage, [&](QStringList args){
+	QObject::connect(&instance, &QSingleInstance::instanceMessage, [&](const QStringList &args){
 		if(!parser.parse(args) || !parser.isSet(QStringLiteral("f")))
 			return;
 		PluginLoader::plugin()->forwardedArguments(parser.positionalArguments());
@@ -84,5 +119,10 @@ static void setupParser(QCommandLineParser &parser)
 	parser.addOption({
 						 {QStringLiteral("f"), QStringLiteral("forward")},
 						 QCoreApplication::translate("GLOBAL", "Forwards the arguments to the plugin")
+					 });
+	parser.addOption({
+						 {QStringLiteral("stdin")},
+						 QCoreApplication::translate("GLOBAL", "Arguments to be forwarded to the plugin should be read from stdin "
+						 "and appended to the commandline")
 					 });
 }
